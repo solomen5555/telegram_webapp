fa050920c7f6974830825fe21d00daa96dc73330
Nguyen Thanh Dat <dat@corsivalab.space>
2024-04-15 10:27:39 +0700

    add bot.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import WebApp from '@twa-dev/sdk'
+
+// const tele = window.Telegram.WebApp;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  // useEffect(() => {
+  //   tele.ready();
+  // }, [])
 
   return (
     <>
@@ -10,6 +17,11 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+          Show alert in telegram
         </button>
       </div>
     </>
---
+++ src/bot/bot.ts
@@ -0,0 +1,15 @@
+// import { Telegraf } from 'telegraf'
+// const TOKEN = "6650488808:AAHIiGFkqKVkOvjpD19Q6366aRKOzFoXlRE";
+// const bot = new Telegraf(TOKEN);
+
+// const web_link = "https://celebrated-torte-184681.netlify.app/";
+
+// bot.start((ctx) =>
+//     ctx.reply("Welcome :)))))", {
+//         reply_markup: {
+//             keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+//         },
+//     })
+// );
+
+// bot.launch();
